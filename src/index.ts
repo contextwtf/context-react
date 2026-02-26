ca4c88c8ff6b2ba307336c8d7d20d82c3d4f0b26
Amir <jahlandbus@gmail.com>
2026-02-26 13:19:38 -0800

    chore: scaffold @contextwtf/react package

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+// @contextwtf/react
---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  clean: true,
+  external: [
+    "react",
+    "viem",
+    "wagmi",
+    "@tanstack/react-query",
+    "@contextwtf/sdk",
+  ],
+});
